e7628c062c827fc81fe1451925439ca23139e5f6
Abdallah Yaqub <yaqubabdallah@gmail.com>
2023-04-01 12:48:23 -0400

    Prepared app for deployment

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
